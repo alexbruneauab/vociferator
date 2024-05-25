328ec946f7d668d25f767f01de50e757497ddc32
alexbruneauab <alexbruneauab@gmail.com>
2024-05-25 11:51:01 +0000

    pytorch quickstart tutorial completed

---
+++ model_data.py
@@ -0,0 +1,21 @@
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+#Load the training and test data
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+train_dataloader = DataLoader(training_data, batch_size=64)
+test_dataloader = DataLoader(test_data, batch_size=64)
